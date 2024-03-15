fdc312747acb6b7a6eac14ed569da8a74aa75c64
Jamal Al-Sharjabi <jamal.alsharjabi@wfp.org>
2024-03-15 23:54:10 +0300

    using output

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "google_compute_instance" "nginx_instance" {
     environment = var.environment_map[var.target_environment]
   }
   tags = var.compute-source-tags
- 
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -60,7 +60,7 @@ resource "google_compute_instance" "nginx_instance" {
   }
 }
 
-## WEB1
+# WEB1
 resource "google_compute_instance" "web1" {
   name         = "web1"
   machine_type = var.environment_machine_type[var.target_environment]
